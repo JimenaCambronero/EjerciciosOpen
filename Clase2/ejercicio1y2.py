ad0c35f3af565bb9be73538424b0be11e3cfe113
Jimena Cambronero <jimenacambronero@gmail.com>
2022-08-29 16:53:19 -0300

    Ejercicios Clase 2

---
+++ Clase2/ejercicio1y2.py
@@ -0,0 +1,12 @@
+#crear una variable a la cual asignarás una cadena de texto y la imprimirás por consola, a su vez, tendrás que imprimir también el tipo de la variable por consola.
+
+cadena = 'Hola Soy el ejercicio 1 de la leccion 2.2'
+print(cadena)
+
+print('El tipo de variable es: ', type(cadena))
+
+
+#Modifica la variable del anterior ejercicio en la consola de python y después muestrala por consola para ver la modificación de la variable.
+
+cadena = 'Ahora soy el ejercicio 2 de la leccion 2.2'
+print(cadena)
