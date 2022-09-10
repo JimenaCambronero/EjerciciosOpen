c6e96b38fcb87779649cc9bac92fbe2bf12fc4b9
Jimena Cambronero <jimenacambronero@gmail.com>
2022-09-10 12:59:41 -0300

    Ejercicio 3

---
+++ Clase5/Ejercicio3.py
@@ -0,0 +1,12 @@
+def es_bisiesto(anio):
+  if type(anio) is not int:
+    print('Debes ingresar un numero entero mayor a 1852')
+  elif anio %4 == 0 and (anio %100 != 0 or anio % 400 == 0):
+    print(f'El año {anio} es bisiesto.')
+  else: 
+    print(f'El año {anio} no es bisiesto.')
+ 
+  
+es_bisiesto(1900)
+es_bisiesto (2000)
+es_bisiesto ('Holi no soy un numero')    
